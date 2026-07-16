b8d843e0b6f21bf17097ac1865c2eea8aa37c1eb
chuan_liu_ya <chuan_liu_ya@163.com>
2026-07-16 09:30:24 +0800

    ♻️ refactor: 按目录重组前端 views 页面

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TestPage from '@/views/TestPage.vue'
+import IndexPage from '@/views/index/IndexPage.vue'
+import TestPage from '@/views/test/TestPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: IndexPage,
+    },
     {
       path: '/test',
       name: 'test',
